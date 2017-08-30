0b0b0c4a76e298268a0f67ab7f7aa46468bcc0d6
Santiago Robayo <s.robayo222@uniandes.edu.co>
2017-08-30 16:28:46 -0500

    Simplificacion del JS
    Simplificacion de JS en los if

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ var es1 = true;
 var es2 = true;
 var es3 = true;
 
+//Los ifs pueden ser simplificados de la forma (condicion)?"caso if":"caso else"
+//¨Para la function esconder1() la haria de este modo
+//function esconder1(){
+//	(es2===false)?document.getElementById('esconder2').style.display = 'none';
+//	if(es3===false)?document.getElementById('esconder3').style.display = 'none';
+//document.getElementById('esconder1').style.display = 'block';
+//es1 = false;
+//}
+// esto ayuda al usuario a la hora de cargar la pagina
 function esconder1(){
 	if(es2===false){
 		document.getElementById('esconder2').style.display = 'none';
